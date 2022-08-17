e4d0070568f9743461443de6bfe243ef69a356d3
chandan behera <ccbehera5@gmail.com>
2022-08-17 20:40:45 +0530

    Office Employee Management System

---
+++ emp_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from emp_app.models import Employee,Role,Department
+  
+admin.site.register(Employee)
+admin.site.register(Role)
+admin.site.register(Department)
